c314a1cb939d6850b72b23f330ea16f1bbea818f
rzhang57 <rmzhang@student.ubc.ca>
2026-02-08 08:38:08 -0800

    add mouse activity

--- server/services/keyboard_service.py
+++ server/services/keyboard_service.py
@@ -1,18 +1,19 @@
 import threading
 import time
 
-from pynput import keyboard
+from pynput import keyboard, mouse
 
-DISENGAGEMENT_THRESHOLD = 20
+DISENGAGEMENT_THRESHOLD = 5
 
 
 class KeyboardService:
     def __init__(self):
         self._last_activity = time.time()
         self._lock = threading.Lock()
-        self._listener = None
+        self._keyboard_listener = None
+        self._mouse_listener = None
 
-    def _on_press(self, key):
+    def _update_activity(self, *args, **kwargs):
         with self._lock:
             self._last_activity = time.time()
 
@@ -22,17 +23,25 @@ class KeyboardService:
             return time.time() - self._last_activity > DISENGAGEMENT_THRESHOLD
 
     def start(self):
-        if self._listener is not None:
+        if self._keyboard_listener is not None:
             return
         with self._lock:
             self._last_activity = time.time()
-        self._listener = keyboard.Listener(on_press=self._on_press)
-        self._listener.daemon = True
-        self._listener.start()
+        self._keyboard_listener = keyboard.Listener(on_press=self._update_activity)
+        self._keyboard_listener.daemon = True
+        self._keyboard_listener.start()
+        self._mouse_listener = mouse.Listener(
+            on_click=self._update_activity,
+            on_move=self._update_activity,
+            on_scroll=self._update_activity,
+        )
+        self._mouse_listener.daemon = True
+        self._mouse_listener.start()
 
     def stop(self):
-        if self._listener is None:
-            return
-        self._listener.stop()
-        self._listener.join(timeout=2)
-        self._listener = None
+        for listener in (self._keyboard_listener, self._mouse_listener):
+            if listener is not None:
+                listener.stop()
+                listener.join(timeout=2)
+        self._keyboard_listener = None
+        self._mouse_listener = None
